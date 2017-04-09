4e2bc8c296fcf088d8ed8fd88fce49bc4dba89ad
Igor <igor.k.kolodziejczyk@gmail.com>
2017-04-09 20:36:26 +0200

    <7 kyu> Fuel economy converter (mpg <-> L/100 km)

---
+++ CodeWars/Kata/Fuel economy converter/FuelEconomyConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata
+{
+    class FuelEconomyConverter
+    {
+        public static double gallon = 3.785411784;
+        public static double mile = 1.609344;
+
+        public static double mpg2lp100km(double x)
+        {
+            return RoundToTwoDecimals(235.21 / x);
+        }
+
+        public static double lp100km2mpg(double x)
+        {
+            return RoundToTwoDecimals(235.21 / x);
+        }
+
+        private static double RoundToTwoDecimals(double numberToRound)
+        {
+            return Math.Round(numberToRound, 2, MidpointRounding.AwayFromZero);
+        }
+    }
+}
---
+++ CodeWars/Kata/Test/FuelEconomyConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    public class FuelEconomyConverterTest
+    {
+
+        [Test]
+        public void lp100km2mpgTest()
+        {
+            Assert.AreEqual(26.13, FuelEconomyConverter.lp100km2mpg(9));
+        }
+
+        [Test]
+        public void mpg2lp100kmTest()
+        {
+            Assert.AreEqual(5.6, FuelEconomyConverter.mpg2lp100km(42));
+        }
+    }
+}
