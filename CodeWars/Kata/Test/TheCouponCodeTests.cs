c40edf8a41646f8247cb3a172753ee40fd11ce36
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-04-12 15:36:00 +0200

    <7 kyu> The Coupon Code

---
+++ CodeWars/Kata/Test/TheCouponCodeTests.cs
@@ -0,0 +1,21 @@
+﻿using CodeWars.Kata.The_Coupon_Code;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    class TheCouponCodeTests
+    {
+        [Test]
+        public static void TheCouponCodeTest()
+        {
+            Assert.AreEqual(true, TheCouponCode.CheckCoupon("123", "123", "September 5, 2014", "October 1, 2014"));
+
+            Assert.AreEqual(false, TheCouponCode.CheckCoupon("123a", "123", "September 5, 2014", "October 1, 2014"));
+        }
+    }
+}
---
+++ CodeWars/Kata/The Coupon Code/TheCouponCode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.The_Coupon_Code
+{
+    class TheCouponCode
+    {
+
+        public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
+        {
+            bool couponStatus;
+
+            couponStatus = CheckCouponCode(enteredCode, correctCode) && CheckCouponeDate(currentDate,expirationDate);
+
+            return couponStatus;
+        }
+
+        private static bool CheckCouponCode(string enteredCode, string correctCode)
+        {
+            return enteredCode == correctCode;
+        }
+
+        private static bool CheckCouponeDate(string currentDate, string expirationDate)
+        {
+            DateTime currentInDateFormat = Convert.ToDateTime(currentDate);
+            DateTime expirationInDateFormat = Convert.ToDateTime(expirationDate);
+
+            Console.WriteLine(currentInDateFormat + " " + expirationDate);
+
+            return currentInDateFormat<=expirationInDateFormat;
+        }
+
+    }
+}
