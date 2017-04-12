e097202d7a9076b1d10c1df07a606c5543cf6ffb
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-04-12 14:27:21 +0200

    <7 kyu> Ones and Zeros

---
+++ CodeWars/Kata/Ones and Zeros/OnesAndZeros.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Ones_and_Zeros
+{
+    class OnesAndZeros
+    {
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            return Convert.ToInt32(ChangeArrayIntoString(BinaryArray), 2);
+        }
+
+        private static string ChangeArrayIntoString(int[] arrayToChange)
+        {
+            string temp = "";
+
+            foreach(int number in arrayToChange)
+            {
+                Console.WriteLine(number);
+                temp = temp + number.ToString();
+            }
+            return temp;
+        }
+    }
+}
---
+++ CodeWars/Kata/Test/OnesAndZerosTests.cs
@@ -0,0 +1,27 @@
+﻿using CodeWars.Kata.Ones_and_Zeros;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    class OnesAndZerosTests
+    {
+        int[] Test1 = new int[] { 0, 0, 0, 0 };
+        int[] Test2 = new int[] { 1, 1, 1, 1 };
+        int[] Test3 = new int[] { 0, 1, 1, 0 };
+        int[] Test4 = new int[] { 0, 1, 0, 1 };
+
+        [Test]
+        public void OnesAndZerosTest()
+        {
+            Assert.AreEqual(0, OnesAndZeros.binaryArrayToNumber(Test1));
+            Assert.AreEqual(15, OnesAndZeros.binaryArrayToNumber(Test2));
+            Assert.AreEqual(6, OnesAndZeros.binaryArrayToNumber(Test3));
+            Assert.AreEqual(5, OnesAndZeros.binaryArrayToNumber(Test4));
+        }
+    }
+}
