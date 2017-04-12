1122cb7fceec166fe5a18bf7f2a22f4812a9f164
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-04-12 13:05:07 +0200

    <6 kyu> Find the odd int

---
+++ CodeWars/Kata/Find the odd int/FindTheOddInt.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.Kata.Find_the_odd_int
+{
+    class FindTheOddInt
+    {
+        private static Dictionary<int, int> numbersCount;
+
+        public static int find_it(int[] seq)
+        {
+            numbersCount = new Dictionary<int, int>();
+            for (int i = 0; i < seq.Count(); i++)
+            {
+                if (numbersCount.ContainsKey(seq[i])){
+                    numbersCount[seq[i]] += 1; 
+                }else
+                {
+                    numbersCount.Add(seq[i], 1);
+                }
+            }
+            return ReturnNumberWithOddAppears(seq);
+        }
+
+        private static int ReturnNumberWithOddAppears(int[] seq)
+        {
+            int index = 0;
+            for(int i = 0; i < seq.Count(); i++)
+            {
+                if(numbersCount[seq[i]] % 2 != 0)
+                {
+                    index = seq[i];
+                    break;
+                }
+            }
+            return index;
+        }
+    }
+}
---
+++ CodeWars/Kata/Test/FindTheOddIntTest.cs
@@ -0,0 +1,24 @@
+﻿using CodeWars.Kata.Find_the_odd_int;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    class FindTheOddIntTests
+    {
+        [Test]
+        public void FindTheOddIntTest()
+        {
+            Assert.AreEqual(5, FindTheOddInt.find_it(new[] { 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 }));
+            Assert.AreEqual(-1, FindTheOddInt.find_it(new[] { 1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5 }));
+            Assert.AreEqual(5, FindTheOddInt.find_it(new[] { 20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5 }));
+            Assert.AreEqual(10, FindTheOddInt.find_it(new[] { 10 }));
+            Assert.AreEqual(10, FindTheOddInt.find_it(new[] { 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1 }));
+            Assert.AreEqual(1, FindTheOddInt.find_it(new[] { 5, 4, 3, 2, 1, 5, 4, 3, 2, 10, 10 }));
+        }
+    }
+}
