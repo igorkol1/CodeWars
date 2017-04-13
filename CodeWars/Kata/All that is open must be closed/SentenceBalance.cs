c46d17a92cff36d0f434f5b404f525e2bd6a001b
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-04-13 15:30:15 +0200

    <5 kyu> Gap in Primes + Primes Class to generate prime numbers

---
+++ CodeWars/Kata/All that is open must be closed/SentenceBalance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.All_that_is_open_must_be_closed
+{
+    class SentenceBalance
+    {
+        private string sentence;
+        private string specialsPairs;
+
+        public SentenceBalance(string sentence, string specialsPairs)
+        {
+            Console.WriteLine(sentence);
+            Console.WriteLine(specialsPairs);
+
+            this.sentence = sentence;
+            this.specialsPairs = specialsPairs;
+        }
+
+        public bool IsBalance()
+        {
+            return IsAllSpecialSignExistInSentence();
+
+            for(int i = 0; i < specialsPairs.Length; i = i+2)
+            {
+                int firstSignIndex = sentence.IndexOf(specialsPairs[i]);
+
+            }
+
+        }
+
+        public bool IsAllSpecialSignExistInSentence()
+        {
+            bool status = true;
+
+            foreach(char sign in specialsPairs)
+            {
+                if (!sentence.Contains(sign))
+                {
+                    status = false;
+                    break;
+                }
+            }
+            return status;
+        }
+
+
+
+    }
+}
---
+++ CodeWars/Kata/Gap in Primes/GapInPrimes.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Gap_in_Primes
+{
+    class GapInPrimes
+    {
+        public static long[] Gap(int g, long m, long n)
+        {
+            Primes primes = new Primes();
+
+            long[] gap = null;
+
+            long[] primesNumbers = primes.GetPrimeNumbers(m,n);
+
+            for (int i = 0; i < primesNumbers.Length-1; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(primesNumbers[i]);
+                Console.WriteLine(primesNumbers[i + 1]);
+                Console.WriteLine(primesNumbers[i + 1] - primesNumbers[i]);
+                if (primesNumbers[i]>=m && primesNumbers[i + 1] - primesNumbers[i] == g)
+                {
+                    gap = new long[2];
+                    gap[0] = primesNumbers[i];
+                    gap[1] = primesNumbers[i + 1];
+                    break;
+                }
+            }
+
+            return gap;
+
+        }
+    }
+}
