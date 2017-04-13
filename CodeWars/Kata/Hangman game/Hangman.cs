7721f918f44dc1db607e5731bcda8763228210c2
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-04-13 11:02:43 +0200

    <6 kyu> Hangman game

---
+++ CodeWars/Kata/Hangman game/Hangman.cs
@@ -0,0 +1,163 @@
+﻿using System.Linq;
+
+namespace CodeWars.Kata.Hangman_game
+{
+    public class Hangman
+    {
+        private string word;
+        private char[] guessLetters;
+        private string missedLetters = " # ";
+        private int mistakes = 0;
+        private bool gameEnded = false;
+
+        public Hangman(string word)
+        {
+            this.word = word;
+
+            InitAndFillEmptyGuessLettersArray();
+        }
+
+        public string Guess(char letter)
+        {
+            if (CheckIfLetterExistInWord(letter))
+            {
+                AddLetterToGuessLettersArray(letter);
+            }else
+            {
+                AddLetterToMissedLetters(letter);
+            }
+
+            return ReturnGameState();
+        }
+
+        private void InitAndFillEmptyGuessLettersArray()
+        {
+            guessLetters = new char[word.Length];
+
+            for(int i=0;i< word.Length; i++)
+            {
+                guessLetters[i] = '_';
+            }
+        }
+
+        private string PrintGuessLettersArray()
+        {
+            string tempToPrint = "";
+
+            foreach(char letter in guessLetters)
+            {
+                tempToPrint += letter+" ";
+            }
+
+            return tempToPrint.Remove(tempToPrint.Length-1,1);
+        }
+
+        private bool CheckIfLetterExistInWord(char letter)
+        {
+            return word.Contains(letter);
+        }
+
+        private void AddLetterToGuessLettersArray(char letter)
+        {
+
+            for(int i = 0; i < word.Length; i++)
+            {
+                if(word[i] == letter)
+                {
+                    guessLetters[i] = letter;
+                }
+            }
+
+        }        
+
+        private void AddLetterToMissedLetters(char letter)
+        {
+            if (!missedLetters.Contains(letter))
+            {
+                missedLetters += letter;
+                mistakes++;
+            }
+        }
+
+        private string GetMissedLetters()
+        {
+            if (mistakes > 0)
+            {
+                return missedLetters;
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        private int CheckGameState()
+        {
+            int state = -2;
+
+            if (IsWordGuessed())
+            {
+                if (!gameEnded)
+                {
+                    state = 1;
+                    gameEnded = true;
+                }else
+                {
+                    state = 2;
+                }
+                
+            }
+            else if (mistakes < 7)
+            {
+                state = 0;
+            }
+            else if(mistakes >=7)
+            {
+                if (!gameEnded)
+                {
+                    state = -1;
+                    gameEnded = true;
+                }
+                else
+                {
+                    state = 2;
+                }
+            }
+            return state;
+        }
+
+        private string ReturnGameState()
+        {
+            int state = CheckGameState();
+            switch (state)
+            {
+                case -1:
+                    return "You got hung! The word was " + word + ".";
+                case 0:
+                    return PrintGuessLettersArray() + GetMissedLetters();
+                case 1:
+                    return "You found the word! (" + word + ")";
+                case 2:
+                    return "The game has ended.";
+                default:
+                    return "Error"; 
+            }
+        }
+
+        private bool IsWordGuessed()
+        {
+            bool status = true;
+
+            foreach(char letter in guessLetters)
+            {
+                if (letter.Equals('_'))
+                {
+                    status = false;
+                    break;
+                }
+            }
+            return status;
+        }
+    }
+}
---
+++ CodeWars/Kata/Test/HangmanTests.cs
@@ -0,0 +1,57 @@
+﻿using CodeWars.Kata.Hangman_game;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    [TestFixture]
+    class HangmanTests
+    {
+        [Test]
+        public void HangmanTest1()
+        {
+            Hangman hangman = new Hangman("codet");
+
+            Assert.AreEqual("_ _ _ _ _ # w", hangman.Guess('w'));
+            Assert.AreEqual("_ _ _ _ _ # wa", hangman.Guess('a'));
+            Assert.AreEqual("_ _ _ _ _ # war", hangman.Guess('r'));
+            Assert.AreEqual("_ _ _ _ _ # wars", hangman.Guess('s'));
+            Assert.AreEqual("_ _ _ e _ # wars", hangman.Guess('e'));
+            Assert.AreEqual("_ _ d e _ # wars", hangman.Guess('d'));
+            Assert.AreEqual("_ o d e _ # wars", hangman.Guess('o'));
+            Assert.AreEqual("c o d e _ # wars", hangman.Guess('c'));
+            Assert.AreEqual("c o d e _ # warsx", hangman.Guess('x'));
+            Assert.AreEqual("c o d e _ # warsxy", hangman.Guess('y'));
+            Assert.AreEqual("You got hung! The word was codet.", hangman.Guess('z'));
+        }
+
+        [Test]
+        public void HangmanTest2()
+        {
+            Hangman hangman = new Hangman("wars");
+
+            Assert.AreEqual("w _ _ _", hangman.Guess('w'));
+            Assert.AreEqual("w _ _ _ # u", hangman.Guess('u'));
+            Assert.AreEqual("w _ _ s # u", hangman.Guess('s'));
+            Assert.AreEqual("w a _ s # u", hangman.Guess('a'));
+            Assert.AreEqual("You found the word! (wars)", hangman.Guess('r'));
+            Assert.AreEqual("The game has ended.", hangman.Guess('g'));
+        }
+
+        [Test]
+        public void HangmanTest3()
+        {
+            Hangman hangman = new Hangman("ababab");
+       
+            Assert.AreEqual("a _ a _ a _", hangman.Guess('a'));
+            Assert.AreEqual("a _ a _ a _ # c", hangman.Guess('c'));
+            Assert.AreEqual("You found the word! (ababab)", hangman.Guess('b'));
+        }
+        
+
+    }
+}
