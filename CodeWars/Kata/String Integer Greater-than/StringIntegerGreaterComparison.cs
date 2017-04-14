37e42f506c5038d2081ff111c5f8764ee1d94444
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-04-14 13:44:09 +0200

    <6 kyu> String Integer Greater-than (despide all assertions passed solution could not be sendet to final)

---
+++ CodeWars/Kata/String Integer Greater-than/StringIntegerGreaterComparison.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.String_Integer_Greater_than
+{
+    class StringIntegerGreaterComparison
+    {
+        
+        public static bool StringIntGreaterThan(string a, string b)
+        {
+            bool status = false;
+            if (IsStringsIntHaveDiffrentSign(a, b))
+            {
+                status = IsFirstStringIsNegative(a, b);
+            }
+            else
+            {
+                status = CompareStringWithTheSameSign(a, b);
+
+
+                if (IfBothStringAreNegative(a, b) && (a!=b))
+                {
+                    status = !status;
+                }
+
+
+            }
+            return status;
+
+        }
+
+        private static bool CompareStringWithTheSameSign(string a, string b)
+        {
+            bool status;
+
+            if (IsStringsHaveDiffrentLenght(a, b))
+            {
+                status = IsFirstStringLonger(a, b);
+            }
+            else
+            {
+                status = CompareStringIntWithTheSameLenght(a, b);
+            }
+
+            return status;
+        }
+
+        private static bool IsStringsIntHaveDiffrentSign(string a, string b)
+        {
+            return a.Contains('-') ^ b.Contains('-');
+        }
+
+        private static bool IfBothStringAreNegative(string a, string b)
+        {
+            return a.Contains('-') && b.Contains('-');
+        }
+
+        private static bool IsFirstStringIsNegative(string a,string b)
+        {
+            bool status = false;
+            if(a.Contains('-') && !b.Contains('-'))
+            {
+                return false;
+            }
+            else if(!a.Contains('-') && b.Contains('-'))
+            {
+                return true;
+            }
+            return status;
+        }
+
+        private static bool IsStringsHaveDiffrentLenght(string a, string b)
+        {
+            return a.Length != b.Length;
+        }
+
+        private static bool IsFirstStringLonger(string a,string b)
+        {
+            return a.Length > b.Length;
+        }
+
+        private static bool CompareStringIntWithTheSameLenght(string a,string b)
+        {
+            bool status = false;
+
+            a = a.Replace('-', '0');
+            b = b.Replace('-', '0');
+
+            for (int i = 0; i< a.Length; i++)
+            {
+                int numberFromA = Convert.ToInt16(Convert.ToString(a[i]));
+                int numberFromB = Convert.ToInt16(Convert.ToString(b[i]));
+
+                if (numberFromA > numberFromB)
+                {
+                    status = true;
+                    break;
+                }else if(numberFromA < numberFromB)
+                {
+                    break;
+                }
+            }
+            return status;
+        }
+    }
+}
---
+++ CodeWars/Kata/Test/StringIntegerGreaterComparisonTests.cs
@@ -0,0 +1,34 @@
+﻿using CodeWars.Kata.String_Integer_Greater_than;
+using NUnit.Framework;
+using System;
+
+
+namespace CodeWars.Kata.Test
+{
+    [TestFixture]
+    class StringIntegerGreaterComparisonTests
+    {
+        [Test]
+        public void StringIntegerGreaterComparisonTest()
+        {
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("31", "162"), "Assert 1");
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("16", "162"), "Assert 2");
+            Assert.IsTrue(StringIntegerGreaterComparison.StringIntGreaterThan("162", "54"), "Assert 3");
+
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("162", "162"), "Assert 4");
+
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("230", "231"), "Assert 5");
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("230", "241"), "Assert 6");
+
+            Assert.IsTrue(StringIntegerGreaterComparison.StringIntGreaterThan("162", "154"),"Assert 7");
+
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("3298796435132165154896798416516156", "3329879643513216515489679841651615"), "Assert 8");
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("-1329879643513216515489679841651615", "-956189648947395809432803247598442"), "Assert 8,5");
+
+            Assert.IsFalse(StringIntegerGreaterComparison.StringIntGreaterThan("-1", "3"), "Assert 9");
+            Assert.IsTrue(StringIntegerGreaterComparison.StringIntGreaterThan("2", "-1"), "Assert 10");
+            Assert.IsTrue(StringIntegerGreaterComparison.StringIntGreaterThan("-1", "-4"), "Assert 10");
+
+        }
+    }
+}
