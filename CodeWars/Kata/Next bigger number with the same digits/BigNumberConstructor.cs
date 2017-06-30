1e65a11015830f00cc7b7b9b9879bc24060c5c41
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-06-30 15:59:49 +0200

    Next Bigger Number Init

---
+++ CodeWars/Kata/Next bigger number with the same digits/BigNumberConstructor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Next_bigger_number_with_the_same_digits
+{
+    class BigNumberConstructor
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            int[] tempArr = TransformLongToArray(n);
+            string tempStr = "";
+
+            for(int i = tempArr.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(tempArr[i]);
+                tempStr += tempArr[i];
+                Console.WriteLine("New number: "+tempStr);
+            }
+
+            return ConvertStringToLong(tempStr);
+        }
+
+        public static int[] TransformLongToArray(long n)
+        {
+            string tempStr = n.ToString();
+            int[] tempArr = new int[tempStr.Length];
+
+            for(int i = 0; i < tempStr.Length; i++)
+            {
+                tempArr[i] = Convert.ToInt32(Char.GetNumericValue(tempStr[i]));
+            }
+
+            return SortArray(tempArr);
+        }
+
+        private static int[] SortArray(int[] arr)
+        {
+            Array.Sort(arr);
+            return arr;
+        }
+
+        private static long ConvertStringToLong(string strToConvert)
+        {
+            return Convert.ToInt64(strToConvert);
+        }
+    }
+}
---
+++ CodeWars/Kata/Test/BigNumberConstructorTests.cs
@@ -0,0 +1,52 @@
+﻿using CodeWars.Kata.Next_bigger_number_with_the_same_digits;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    [TestFixture]
+    class BigNumberConstructorTests
+    {
+        [Test]
+        public void TransformLongToArrayTest()
+        {
+            Console.WriteLine("12");
+            int[] tempArr = new int[]{ 1, 2 };
+            Assert.AreEqual(tempArr, BigNumberConstructor.TransformLongToArray(12));
+
+            Console.WriteLine("513");
+            tempArr = new int[]{ 1, 3, 5};
+            Assert.AreEqual(tempArr, BigNumberConstructor.TransformLongToArray(513));
+
+            Console.WriteLine("2017");
+            tempArr = new int[] { 0, 1, 2, 7};
+            Assert.AreEqual(tempArr, BigNumberConstructor.TransformLongToArray(2017));
+
+            Console.WriteLine("414");
+            tempArr = new int[] { 1, 4, 4 };
+            Assert.AreEqual(tempArr, BigNumberConstructor.TransformLongToArray(414));
+
+            Console.WriteLine("144");
+            tempArr = new int[] { 1, 4, 4 };
+            Assert.AreEqual(tempArr, BigNumberConstructor.TransformLongToArray(144));
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("****** Small Number");
+            Assert.AreEqual(21, BigNumberConstructor.NextBiggerNumber(12));
+            Assert.AreEqual(531, BigNumberConstructor.NextBiggerNumber(513));
+            Assert.AreEqual(2071, BigNumberConstructor.NextBiggerNumber(2017));
+            Assert.AreEqual(441, BigNumberConstructor.NextBiggerNumber(414));
+            Assert.AreEqual(414, BigNumberConstructor.NextBiggerNumber(144));
+        }
+
+
+    }
+}
