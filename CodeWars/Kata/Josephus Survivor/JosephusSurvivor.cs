3b76301e38c9d3e038bd626de80eed12709fdeb2
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-07-14 14:24:32 +0200

    Josephus Survivor <6 kyu> DONE

---
+++ CodeWars/Kata/Josephus Survivor/JosephusSurvivor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Josephus_Survivor
+{
+    class JosephusSurvivor
+    {
+
+        public static int JosSurvivor(int n, int k)
+        {
+            List<int> people = new List<int>(CreateArray(n));
+            int current = k - 1;
+
+            current = current % people.Count;
+
+            while (people.Count > 1)
+            {
+               
+                people.RemoveAt(current);
+
+                current += k - 1;
+
+                current = current % people.Count; 
+            }
+            return people[0];
+        }
+
+        
+        private static int[] CreateArray(int n)
+        {
+            int[] arr = new int[n];
+
+            return FillArrayWithNatural(arr);
+        }
+
+        private static int[] FillArrayWithNatural(int[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 1;
+            }
+
+            return arr;
+        }
+        
+    }
+}
---
+++ CodeWars/Kata/Test/JosephusSurvivorTests.cs
@@ -0,0 +1,36 @@
+﻿using CodeWars.Kata.Josephus_Survivor;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    [TestFixture]
+    class JosephusSurvivorTests
+    {
+        private static void testing(int actual, int expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Basic Tests JosSurvivor");
+            testing(JosephusSurvivor.JosSurvivor(7, 3), 4);
+            testing(JosephusSurvivor.JosSurvivor(11, 19), 10);
+            testing(JosephusSurvivor.JosSurvivor(40, 3), 28);
+            testing(JosephusSurvivor.JosSurvivor(14, 2), 13);
+            testing(JosephusSurvivor.JosSurvivor(100, 1), 100);
+            testing(JosephusSurvivor.JosSurvivor(1, 300), 1);
+            testing(JosephusSurvivor.JosSurvivor(2, 300), 1);
+            testing(JosephusSurvivor.JosSurvivor(5, 300), 1);
+            testing(JosephusSurvivor.JosSurvivor(7, 300), 7);
+            testing(JosephusSurvivor.JosSurvivor(300, 300), 265);
+        }
+
+    }
+}
