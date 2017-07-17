f97f6796e3387dd74217d1867e501f3980b58767
ikolodziejczyk <ikolodziejczyk@pl.sii.eu>
2017-07-17 09:40:36 +0200

    Bit Calculator <5 kyu> DONE

---
+++ CodeWars/Kata/Bit calculator/BitCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Bit_calculator
+{
+    class BitCalculator
+    {
+        public static int Calculate(string num1, string num2)
+        {
+            int intNumberA = Convert.ToInt32(num1,2);
+            int intNumberB = Convert.ToInt32(num2,2);
+
+            Console.WriteLine(intNumberA + " " + intNumberB);
+
+            return intNumberA+intNumberB;
+        }
+    }
+}
---
+++ CodeWars/Kata/Test/BitCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using CodeWars.Kata.Bit_calculator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Kata.Test
+{
+    [TestFixture]
+    class BitCalculatorTests
+    {
+
+        [Test]
+        public void BitCalculatorTest()
+        {
+            Assert.AreEqual(2, BitCalculator.Calculate("1", "1"));
+            Assert.AreEqual(4, BitCalculator.Calculate("10", "10"));
+            Assert.AreEqual(2, BitCalculator.Calculate("10", "0"));
+            Assert.AreEqual(3, BitCalculator.Calculate("10", "1"));
+        }
+
+    }
+}
